ce872c0f4cfb1e8f49fac1a11c7ede888f616a7c
MajorX234 <majorx234@googlemail.com>
2024-04-07 23:58:00 +0200

    Add skeleton for volume slider widget

---
+++ src/volume_slider/volume_slider.cpp
@@ -0,0 +1,5 @@
+#include "volume_slider"
+VolumeSlider::VolumeSlider(QWidget *parent){}
+void VolumeSlider::setValue(int value){}
+void VolumeSlider::setMinimum(int value){}
+void VolumeSlider::setMaximum(int value){}
